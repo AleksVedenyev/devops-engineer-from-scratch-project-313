cfe9f0e5b8bcff6d086abf6430b35bbcd3daa692
AleksVedenyev <aleks.vedenev@gmail.com>
2026-07-07 20:08:47 +0300

    Fix postgres URL for SQLAlchemy

--- main.py
+++ main.py
@@ -20,6 +20,13 @@ load_dotenv()
 DATABASE_URL = os.getenv('DATABASE_URL', "sqlite:///database.db")
 BASE_URL = os.getenv('BASE_URL')
 
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace(
+        "postgres://",
+        "postgresql://",
+        1
+    )
+
 engine = create_engine(DATABASE_URL)
 
 
